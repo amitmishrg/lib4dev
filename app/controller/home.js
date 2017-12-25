70cd2eca67acd97a6900e93ad586e19243c4939a
amituidev <amitmishra009v@gmail.com>
2017-12-25 19:43:03 +0530

    filter issue fixes

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,27 +3,29 @@ const constant = require('../constant/index');
 const common = require('../utils/common');
 
 let init = async (req, res) => {
-  console.log(req);
   try {
     let topic = common.getTopic(req);
     let url = common.constructHomeApiEndPoint(req);
     let response = await Home.init(url);
-    let perPage = constant.pagination.perPage;
-    let pageNumber = Math.ceil(response.total_count / perPage);
-    let pageData = {
-      topic: topic,
-      sort: req.query.s || '',
-      searchTerm: req.query.search || '',
-      totalCount: response.total_count,
-      items: response.items,
-      languages: constant.language,
-      tags: constant.tags,
-      options: constant.options,
-      pages: pageNumber,
-      current: req.query.p || 1
+    if (response) {
+      let perPage = constant.pagination.perPage;
+      let pageNumber = Math.ceil(response.total_count / perPage);
+      let pageData = {
+        topic: topic,
+        sort: req.query.s || '',
+        searchTerm: req.query.search || '',
+        totalCount: response.total_count,
+        items: response.items,
+        languages: constant.language,
+        tags: constant.tags,
+        options: constant.options,
+        pages: pageNumber,
+        current: req.query.p || 1
+      }
+      res.render('home', pageData);
+      console.log(pageData);
     }
-    console.log(pageData);
-    res.render('home', pageData);
+
   } catch (error) {
     console.log(error);
   }
